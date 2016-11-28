e05f8e4e30a3c0f4659a411ada55d931750affcf
rabindratharu <rabindra.tharu.np@gmail.com>
2016-11-28 17:17:16 +0545

    Migrated the custom css box settings to additional css section introduced in WordPress 4.7

--- inc/extras.php
+++ inc/extras.php
@@ -137,3 +137,24 @@ function radiate_wrapper_start() {
 function radiate_wrapper_end() {
   echo '</div>';
 }
+
+/**
+ * Migrate any existing theme CSS codes added in Customize Options to the core option added in WordPress 4.7
+ */
+function radiate_customm_css_migrate() {
+	if ( get_option( 'radiate_custom_css_transfer' ) ) {
+		return;
+	}
+	$theme_custom_css = get_theme_mod( 'radiate_custom_css', '' );
+	if ( ! empty( $theme_custom_css ) && function_exists( 'wp_update_custom_css_post' ) ) {
+		$wordpress_core_css = wp_get_custom_css(); // Preserve any CSS already added to the core option.
+		$return = wp_update_custom_css_post( $wordpress_core_css . $theme_custom_css );
+		if ( ! is_wp_error( $return ) ) {
+			// Set the transfer as complete
+			update_option( 'radiate_custom_css_transfer', 1 );
+			// Remove the old theme_mod option for the Custom CSS Box provided via theme
+			remove_theme_mod( 'radiate_custom_css' );
+		}
+	}
+}
+add_action( 'after_setup_theme', 'radiate_customm_css_migrate' );
