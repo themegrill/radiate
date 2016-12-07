500cadff2a4b53131d95e0dd500fa1ebeed94750
Romos <romosamatya17@gmail.com>
2016-12-07 10:21:23 +0545

    Removes unwanted code from custom.js file

--- js/custom.js
+++ js/custom.js
@@ -41,12 +41,6 @@ function header_image_effect() {
 }
 
 jQuery(document).ready(function() {
-    // jQuery('.better-responsive-menu #site-navigation .menu-item-has-children').append('<span class="sub-toggle"> <span class="genericon genericon-expand"></span> </span>');
-    // jQuery('.better-responsive-menu #site-navigation .sub-toggle').click(function() {
-    //     jQuery(this).parent('.menu-item-has-children').children('ul.sub-menu').first().slideToggle('1000');
-    //     jQuery(this).children('<span class="genericon genericon-rightarrow"></span>').first().toggleClass('<span class="genericon genericon-expand"></span>');
-    //     jQuery(this).toggleClass('active');
-    // });
     jQuery('.better-responsive-menu  #site-navigation .menu-toggle').click(function() {
       jQuery('.better-responsive-menu  #site-navigation .menu').slideToggle('slow');
     });
