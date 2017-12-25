85205db0bdb2d31742a173f6cb3e6a3bdf478f55
Nima <nima.themegrill@gmail.com>
2017-12-25 15:24:30 +0545

    Selective refresh for site title and description

--- inc/customizer.php
+++ inc/customizer.php
@@ -12,6 +12,18 @@ function radiate_register_theme_customizer( $wp_customize ) {
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 
+	if ( isset( $wp_customize->selective_refresh ) ) {
+      $wp_customize->selective_refresh->add_partial( 'blogname', array(
+         'selector'        => '.site-title a',
+         'render_callback' => 'radiate_customize_partial_blogname',
+      ) );
+
+      $wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+         'selector'        => '.site-description',
+         'render_callback' => 'radiate_customize_partial_blogdescription',
+      ) );
+   }
+
 	// remove control
 	$wp_customize->remove_control('blogdescription');
 
@@ -348,6 +360,25 @@ function radiate_customize_preview_js() {
 	wp_enqueue_script( 'radiate_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
 }
 add_action( 'customize_preview_init', 'radiate_customize_preview_js' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function radiate_customize_partial_blogname() {
+   bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function radiate_customize_partial_blogdescription() {
+   bloginfo( 'description' );
+}
+
 
 if ( ! function_exists( 'radiate_darkcolor' ) ) :
 /**
