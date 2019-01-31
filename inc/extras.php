40b636663ca502eea597de7dde9db9abc34e7a10
Nitu Shrestha <shresthanitu984@gmail.com>
2019-01-31 16:31:29 +0545

    Modify pingback url autodiscovery for single posts, pages, or attachments.

--- inc/extras.php
+++ inc/extras.php
@@ -206,3 +206,18 @@ function radiate_custom_css_migrate() {
 
 }
 add_action( 'after_setup_theme', 'radiate_custom_css_migrate' );
+
+if ( ! function_exists( 'radiate_pingback_header' ) ) :
+
+	/**
+	 * Add a pingback url auto-discovery header for single posts, pages, or attachments.
+	 */
+	function radiate_pingback_header() {
+		if ( is_singular() && pings_open() ) {
+			printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
+		}
+	}
+
+endif;
+
+add_action( 'wp_head', 'radiate_pingback_header' );
--- readme.txt
+++ readme.txt
@@ -64,6 +64,9 @@ If you want to translate this theme to your language, Please visit:- https://tra
 /**********************************************************/
 
 == CHANGE LOG ==
+= TBD =
+* Tweak - Modify pingback url autodiscovery for single posts, pages, or attachments.
+
 = Version 1.3.0 -2019-01-14 =
 * Fix - Gallery padding for Gutenberg
 * Recommend - Zakra theme
