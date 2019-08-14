f7c2b442dde063e8e16e03267215d6ad00dab1a5
Bishal Napit <mebishalnapit@gmail.com>
2019-08-14 11:11:03 +0545

    Add - Focus class addition for menu on keyboard Tab button press

--- header.php
+++ header.php
@@ -56,7 +56,6 @@ if ( function_exists( 'wp_body_open' ) ) {
 
 			<nav id="site-navigation" class="main-navigation" role="navigation">
 				<h4 class="menu-toggle"></h4>
-				<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'radiate' ); ?></a>
 
 				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container_class' => 'menu-primary-container' ) ); ?>
 			</nav><!-- #site-navigation -->
