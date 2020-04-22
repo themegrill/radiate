068b1446f89e33d9b04e9cfcf9216e8b038c4871
NituShrestha <shresthanitu984@gmail.com>
2020-04-22 12:28:46 +0545

    Remove Global declaration of $Current_user

--- inc/admin/class-radiate-theme-review-notice.php
+++ inc/admin/class-radiate-theme-review-notice.php
@@ -54,8 +54,7 @@ class Radiate_Theme_Review_Notice {
 	 */
 	public function radiate_theme_review_notice() {
 
-		global $current_user;
-		$user_id                  = $current_user->ID;
+		$user_id                  = get_current_user_id();
 		$current_user             = wp_get_current_user();
 		$ignored_notice           = get_user_meta( $user_id, 'radiate_ignore_theme_review_notice', true );
 		$ignored_notice_partially = get_user_meta( $user_id, 'nag_radiate_ignore_theme_review_notice_partially', true );
@@ -120,8 +119,7 @@ class Radiate_Theme_Review_Notice {
 	public function radiate_ignore_theme_review_notice()
 	{
 
-		global $current_user;
-		$user_id = $current_user->ID;
+		$user_id = get_current_user_id();
 
 		/* If user clicks to ignore the notice, add that to their user meta */
 		if (isset($_GET['nag_radiate_ignore_theme_review_notice']) && '0' === $_GET['nag_radiate_ignore_theme_review_notice']
@@ -136,8 +134,7 @@ class Radiate_Theme_Review_Notice {
 	 */
 	public function radiate_ignore_theme_review_notice_partially() {
 
-		global $current_user;
-		$user_id = $current_user->ID;
+		$user_id = get_current_user_id();
 
 		/* If user clicks to ignore the notice, add that to their user meta */
 		if ( isset( $_GET['nag_radiate_ignore_theme_review_notice_partially'] ) && '0' === $_GET['nag_radiate_ignore_theme_review_notice_partially'] ) {
