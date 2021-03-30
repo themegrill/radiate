bf3d3621e54bd835d2c9dff6d0eeaf8ea58c6bb3
NituShrestha <shresthanitu984@gmail.com>
2021-03-30 11:04:41 +0545

    Add - enqueue scripts

---
+++ inc/enqueue-scripts.php
@@ -0,0 +1,188 @@
+<?php
+/**
+ * Radiate enqueue CSS and JS files.
+ *
+ * @package    ThemeGrill
+ * @subpackage Radiate
+ * @since      Radiate 2.3.0
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Enqueue scripts and styles.
+ */
+function radiate_scripts() {
+	// Load our main stylesheet.
+	wp_enqueue_style( 'radiate-style', get_stylesheet_uri() );
+
+	wp_enqueue_style( 'radiate-google-fonts', '//fonts.googleapis.com/css?family=Roboto|Merriweather:400,300' );
+
+	// Add Genericons, used in the main stylesheet.
+	wp_enqueue_style( 'radiate-genericons', get_template_directory_uri() . '/genericons/genericons.css', array(), '3.3.1' );
+
+	wp_enqueue_script( 'radiate-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+
+	wp_enqueue_script( 'radiate-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+	wp_enqueue_script( 'radiate-custom-js', get_template_directory_uri() . '/js/custom.js', array( 'jquery' ), false, true );
+
+	$radiate_header_image_link = get_header_image();
+	wp_localize_script( 'radiate-custom-js', 'radiateScriptParam', array( 'radiate_image_link' => $radiate_header_image_link ) );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+
+	wp_enqueue_script( 'html5shiv', get_template_directory_uri() . '/js/html5shiv.js', array(), '3.7.3', false );
+	wp_script_add_data( 'html5shiv', 'conditional', 'lte IE 8' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'radiate_scripts' );
+
+if ( ! function_exists( 'radiate_parse_css' ) ) :
+
+	/**
+	 * Parses CSS.
+	 *
+	 * @param string|array $default_value Default value.
+	 * @param string|array $output_value Updated value.
+	 * @param array $css_output Array of CSS.
+	 * @param string $min_media Min Media breakpoint.
+	 * @param string $max_media Max Media breakpoint.
+	 *
+	 * @return string Generated CSS.
+	 */
+	function radiate_parse_css( $default_value, $output_value, $css_output = array(), $min_media = '', $max_media = '' ) {
+
+		// Return if default value matches.
+		if ( $default_value == $output_value ) {
+			return;
+		}
+
+		$parse_css = '';
+		if ( is_array( $css_output ) && count( $css_output ) > 0 ) {
+
+			foreach ( $css_output as $selector => $properties ) {
+
+				if ( null === $properties ) {
+					break;
+				}
+
+				if ( ! count( $properties ) ) {
+					continue;
+				}
+
+				$temp_parse_css   = $selector . '{';
+				$properties_added = 0;
+
+				foreach ( $properties as $property => $value ) {
+
+					if ( '' === $value ) {
+						continue;
+					}
+
+					$properties_added++;
+					$temp_parse_css .= $property . ':' . $value . ';';
+				}
+
+				$temp_parse_css .= '}';
+
+				if ( $properties_added > 0 ) {
+					$parse_css .= $temp_parse_css;
+				}
+			}
+
+			if ( '' !== $parse_css && ( '' !== $min_media || '' !== $max_media ) ) {
+
+				$media_css       = '@media ';
+				$min_media_css   = '';
+				$max_media_css   = '';
+				$media_separator = '';
+
+				if ( '' !== $min_media ) {
+					$min_media_css = '(min-width:' . $min_media . 'px)';
+				}
+
+				if ( '' !== $max_media ) {
+					$max_media_css = '(max-width:' . $max_media . 'px)';
+				}
+
+				if ( '' !== $min_media && '' !== $max_media ) {
+					$media_separator = ' and ';
+				}
+
+				$media_css .= $min_media_css . $media_separator . $max_media_css . '{' . $parse_css . '}';
+
+				return $media_css;
+
+			}
+		}
+
+		return $parse_css;
+
+	}
+
+endif;
+
+if ( ! function_exists( 'radiate_parse_background_css' ) ) :
+
+	/**
+	 * Returns the background CSS property for dynamic CSS generation.
+	 *
+	 * @param string|array $default_value Default value.
+	 * @param string|array $output_value Updated value.
+	 * @param string $selector CSS selector.
+	 *
+	 * @return string|void Generated CSS for background CSS property.
+	 */
+	function radiate_parse_background_css( $default_value, $output_value, $selector ) {
+
+		if ( $default_value == $output_value ) {
+			return;
+		}
+
+		$parse_css = '';
+		$parse_css .= $selector . '{';
+
+		// For background color.
+		if ( isset( $output_value['background-color'] ) && ( $output_value['background-color'] != $default_value['background-color'] ) ) {
+			$parse_css .= 'background-color:' . $output_value['background-color'] . ';';
+		}
+
+		// For background image.
+		if ( isset( $output_value['background-image'] ) && ( $output_value['background-image'] != $default_value['background-image'] ) ) {
+			$parse_css .= 'background-image:url(' . $output_value['background-image'] . ');';
+		}
+
+		// For background position.
+		if ( isset( $output_value['background-position'] ) && ( $output_value['background-position'] != $default_value['background-position'] ) ) {
+			$parse_css .= 'background-position:' . $output_value['background-position'] . ';';
+		}
+
+		// For background size.
+		if ( isset( $output_value['background-size'] ) && ( $output_value['background-size'] != $default_value['background-size'] ) ) {
+			$parse_css .= 'background-size:' . $output_value['background-size'] . ';';
+		}
+
+		// For background attachment.
+		if ( isset( $output_value['background-attachment'] ) && ( $output_value['background-attachment'] != $default_value['background-attachment'] ) ) {
+			$parse_css .= 'background-attachment:' . $output_value['background-attachment'] . ';';
+		}
+
+		// For background repeat.
+		if ( isset( $output_value['background-repeat'] ) && ( $output_value['background-repeat'] != $default_value['background-repeat'] ) ) {
+			$parse_css .= 'background-repeat:' . $output_value['background-repeat'] . ';';
+		}
+
+		$parse_css .= '}';
+
+		return $parse_css;
+
+	}
+
+endif;
